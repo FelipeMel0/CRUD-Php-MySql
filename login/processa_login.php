25b006cf09090f6301309b430e728ef647cce249
FelipeMel0 <felipedemfernandes@gmail.com>
2021-11-12 10:20:52 -0300

    Testando funções

--- login/processa_login.php
+++ login/processa_login.php
@@ -4,11 +4,16 @@ include('./acoes.php');
 
 session_start();
 
-if (isset($_POST['txt_usuario']) || isset($_POST['txt_senha'])) {
+if (isset($_POST['txt_usuario']) || isset($_POST['txt_senha']) && $_POST['txt_usuario'] !== "") {
 
     $usuario = $_POST['txt_usuario'];
     $senha = $_POST['txt_senha'];
 
+    echo "Usuário: " . $usuario;
+    echo "<br/>";
+    echo "Senha: " . $senha;
+    // header("location: ../listagem/index.php");
+} else {
+    header("location: ./index.php");
 }
 
-header("location: ../listagem/index.php");
